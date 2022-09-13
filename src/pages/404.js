324ab1d8efff42d83879c13162a97183693af344
loidolt <loidolt@gmail.com>
2022-09-13 17:12:24 -0600

    404 page tweaks

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,10 +5,11 @@ import {
   List,
   ListItem,
   ListItemText,
-  Typography,
+  Stack,
 } from "@mui/material"
 
 import { Seo, Layout, MainWrapper, Link } from "components"
+import { Info } from "sections"
 
 const NotFoundPage = () => (
   <Layout>
@@ -24,71 +25,57 @@ const NotFoundPage = () => (
           minHeight: "46vh",
         }}
       >
-        <Typography
-          variant="h1"
+        <Stack
+          justifyContent="center"
+          alignItems="center"
         >
-          Off the Trail
-        </Typography>
-        <Typography
-          variant="h5"
-          component="h2"
-        >
-          We can't seem to find the page you're looking for
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          sx={{
-            fontWeight: 400,
-            color: "#7a7a7a",
-            paddingBottom: "1em",
-          }}
-        >
-          Error code: 404
-        </Typography>
-        <Typography variant="h6">
-          Here are some helpful links instead:
-        </Typography>
-        <Paper
-          sx={{
-            marginTop: "1em",
-            marginBottom: "1em",
-            width: "100%",
-            maxWidth: 360,
-          }}
-        >
-          <List component="nav">
-            <Link to="/">
-              <ListItem button>
-                <ListItemText primary="Homepage" />
-              </ListItem>
-            </Link>
-            <Link to="/contact-us">
-              <ListItem button>
-                <ListItemText primary="Contact Us" />
-              </ListItem>
-            </Link>
-            <Link to="/explore">
-              <ListItem button>
-                <ListItemText primary="About Us" />
-              </ListItem>
-            </Link>
-            <Link to="/collections/hammocks">
-              <ListItem button>
-                <ListItemText primary="Hammocks" />
-              </ListItem>
-            </Link>
-            <Link to="/collections/tree-straps">
-              <ListItem button>
-                <ListItemText primary="Tree Straps" />
-              </ListItem>
-            </Link>
-            <Link to="/account">
-              <ListItem button>
-                <ListItemText primary="Your Account" />
-              </ListItem>
-            </Link>
-          </List>
-        </Paper>
+          <Info title="404: Off the Trail">
+            We can't seem to find the page you're looking for.
+            <br /> <br />
+            <b>Links to get you moving again:</b>
+          </Info>
+          <Paper
+            sx={{
+              marginTop: "1em",
+              marginBottom: "1em",
+              width: "100%",
+              maxWidth: 360,
+            }}
+          >
+            <List component="nav">
+              <Link to="/">
+                <ListItem button>
+                  <ListItemText primary="Homepage" />
+                </ListItem>
+              </Link>
+              <Link to="/contact-us">
+                <ListItem button>
+                  <ListItemText primary="Contact Us" />
+                </ListItem>
+              </Link>
+              <Link to="/explore">
+                <ListItem button>
+                  <ListItemText primary="About Us" />
+                </ListItem>
+              </Link>
+              <Link to="/collections/hammocks">
+                <ListItem button>
+                  <ListItemText primary="Hammocks" />
+                </ListItem>
+              </Link>
+              <Link to="/collections/tree-straps">
+                <ListItem button>
+                  <ListItemText primary="Tree Straps" />
+                </ListItem>
+              </Link>
+              <Link to="/account">
+                <ListItem button>
+                  <ListItemText primary="Your Account" />
+                </ListItem>
+              </Link>
+            </List>
+          </Paper>
+        </Stack>
       </Box>
     </MainWrapper>
   </Layout>
