c3713b6d2d8ba589fc56d7992d6b6281471b7fe8
loidolt <loidolt@gmail.com>
2022-09-22 14:51:42 -0600

    Style and typography tweaks

--- src/sections/ProductPage/Inventory.js
+++ src/sections/ProductPage/Inventory.js
@@ -32,7 +32,7 @@ export const Inventory = ({ handle, id }) => {
           ) : inventory?.quantityAvailable > 20 ? (
             ""
           ) : (
-            <Typography sx={{ fontStyle: "italic" }}>0 Item left</Typography>
+            <Typography sx={{ fontStyle: "italic" }}>Out Of Stock</Typography>
           )}
         </>
       )}
--- src/sections/ProductPage/ProductQuantityAdder.js
+++ src/sections/ProductPage/ProductQuantityAdder.js
@@ -1,59 +1,9 @@
 import React, { useState, useContext } from "react"
-import { Box, Typography, styled, Button } from "@mui/material"
+import { Box, Typography, styled, IconButton, TextField, Stack } from "@mui/material"
 import { Add, Remove } from "@mui/icons-material"
 
 import { OnButton } from "components"
 import { CartContext } from "contexts"
-
-const OutofStock = styled(Box)(() => ({
-  marginTop: "30px",
-  maxWidth: "450px",
-  borderRadius: "20px",
-  border: "1px solid #000",
-  padding: "25px 20px",
-}))
-
-const EmailBox = styled(Box)(() => ({
-  display: "flex",
-
-  "& input": {
-    minHeight: "36px",
-    padding: "0 10px 0 5px",
-  },
-}))
-
-const QuantityWrapper = styled(Box)(() => ({
-  margin: "10px 0",
-
-  "& .quantity": {
-    width: "47px",
-    height: "30px",
-    padding: "0 11px",
-    margin: "0 10px",
-    borderRadius: "10px",
-    textAlign: "center",
-  },
-
-  "& input[type=number]": {
-    appearance: "textfield",
-    background: "#fff",
-  },
-
-  "& input[type=number]::-webkit-outer-spin-button": {
-    appearance: "none",
-    margin: 0,
-  },
-
-  "& input[type=number]::-webkit-inner-spin-button ": {
-    appearance: "none",
-    margin: 0,
-  },
-}))
-
-const QuantityButton = styled(Button)({
-  minWidth: 0,
-  minHeight: 0,
-})
 
 export function ProductQuantityAdder({ variantId, available }) {
   const [quantity, setQuantity] = useState(1)
@@ -72,41 +22,31 @@ export function ProductQuantityAdder({ variantId, available }) {
     <Box>
       <Typography variant="navUser">Quantity</Typography>
       <form style={{ marginTop: "10px" }} onSubmit={handleSubmit}>
-        <QuantityWrapper>
-          <QuantityButton
-            variant="outlined"
+        <Stack direction="row" spacing={2}>
+          <IconButton
             color="primary"
             disabled={!available}
             onClick={() => setQuantity(quantity - 1)}
-            sx={{
-              height: "32px",
-              width: "32px",
-              borderRadius: "300px",
-            }}
+            sx={{ height: "57px", width: "57px" }}
           >
-            <Remove fontSize="small" />
-          </QuantityButton>
-          <input
-            className="quantity"
+            <Remove />
+          </IconButton>
+          <TextField
             disabled={!available}
             type="number"
             value={quantity}
             onChange={handleQuantityChange}
+            sx={{ width: "80px" }}
           />
-          <QuantityButton
-            variant="outlined"
+          <IconButton
             color="primary"
             disabled={!available}
             onClick={() => setQuantity(quantity + 1)}
-            sx={{
-              height: "32px",
-              width: "32px",
-              borderRadius: "300px",
-            }}
+            sx={{ height: "57px", width: "57px" }}
           >
-            <Add fontSize="small" />
-          </QuantityButton>
-        </QuantityWrapper>
+            <Add />
+          </IconButton>
+        </Stack>
 
         <OnButton
           margin="30px 0 0 0"
@@ -118,18 +58,30 @@ export function ProductQuantityAdder({ variantId, available }) {
         </OnButton>
       </form>
       {!available && (
-        <OutofStock>
+        <Box
+          sx={{
+            marginTop: "30px",
+            maxWidth: "450px",
+            borderRadius: "20px",
+            border: "1px solid #000",
+            padding: "25px 20px",
+          }}>
           <Typography variant="h6">Out Of Stock</Typography>
           <Typography mb="20px" variant="body1">
             We will notify you when this product becomes available.
           </Typography>
-          <EmailBox>
-            <input placeholder="Email Address" type="Email Address" />
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            spacing={2}
+          >
+            <TextField label="Email Address" type="email" />
             <OnButton padding="4px 20px" type="submit">
               Notify Me
             </OnButton>
-          </EmailBox>
-        </OutofStock>
+          </Stack>
+        </Box>
       )}
     </Box>
   )
