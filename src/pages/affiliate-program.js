f316484aa4008172a2f0717cba33a841909142a6
loidolt <loidolt@gmail.com>
2022-09-23 15:52:56 -0600

    Remove banner from affiliate page

--- src/pages/affiliate-program.js
+++ src/pages/affiliate-program.js
@@ -1,28 +1,10 @@
 import React from "react"
-import { StaticImage } from "gatsby-plugin-image"
 
 import { Seo, Layout, IframeWrap } from "components"
-import { Hero } from "sections"
-
-const heroData = {
-  position: "center",
-  mainText: "AFFILIATE PROGRAM",
-}
 
 const AffiliateProgramPage = () => (
   <Layout>
-    <Seo />
-    <Hero data={heroData}>
-      <StaticImage
-        style={{
-          gridArea: "1/1",
-        }}
-        layout="fullWidth"
-        alt="Ultralight Hammock"
-        src="../assets/images/pro-deal/pro-deal.jpg"
-        placeholder="blurred"
-      />
-    </Hero>
+    <Seo title="Affiliate Program " />
 
     <IframeWrap>
       <iframe src="https://www.shoutout.global/signup?id=nq1rq" width="100%" height="900px" frameborder="no" title="Affiliate Program Form"></iframe>
