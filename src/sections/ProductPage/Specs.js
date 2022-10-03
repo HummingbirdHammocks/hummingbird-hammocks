bee6e0259bd07ddb32d19bea4839c7c777811119
loidolt <loidolt@gmail.com>
2022-10-02 20:57:33 -0600

    Specs use paragraph tags

--- src/sections/ProductPage/Specs.js
+++ src/sections/ProductPage/Specs.js
@@ -51,55 +51,48 @@ export const Specs = ({ metas, top }) => {
         <Typography marginBottom={2} variant="h5">
           Packed
         </Typography>
-        <Typography variant="body1">
+        <Typography variant="body1" component={"div"}>
           {weight && (
-            <>
+            <p>
               <b>Weight:</b> {`${weight} oz (${Number(parseFloat(weight * 28.3495)).toFixed(1)} g)`}
-              <br />
-            </>
+            </p>
           )}
           {length[1] && (
-            <>
+            <p>
               <b>Length:</b> {`${length[1]} in (${Number(parseFloat(length[1] * 2.54)).toFixed(1)} cm)`}
-              <br />
-            </>
+            </p>
           )}
           {width[1] && (
-            <>
+            <p>
               <b>Width:</b> {`${width[1]} in (${Number(parseFloat(width[1] * 2.54)).toFixed(1)} cm)`}
-              <br />
-            </>
+            </p>
           )}
           {height && (
-            <>
+            <p>
               <b>Height:</b> {`${height} in (${Number(parseFloat(height * 2.54)).toFixed(1)} cm)`}
-              <br />
-            </>
+            </p>
           )}
         </Typography>
       </Grid>
       <Grid item xs={12} lg={6}>
-        <Typography marginBottom={2} variant="h5">
+        <Typography marginBottom={2} variant="h5" component={"div"}>
           In Use
         </Typography>
         <Typography variant="body1">
           {weightRating && (
-            <>
+            <p>
               <b>Weight Rating:</b> {`${weightRating} lbs (${Number(parseFloat(weightRating * 0.453592)).toFixed(1)} kg)`}
-              <br />
-            </>
+            </p>
           )}
           {length[0] && (
-            <>
+            <p>
               <b>Length:</b> {`${length[0]} in (${Number(parseFloat(length[0] * 2.54)).toFixed(1)} cm)`}
-              <br />
-            </>
+            </p>
           )}
           {width[0] && (
-            <>
+            <p>
               <b>Width:</b> {`${width[0]} in (${Number(parseFloat(width[0] * 2.54)).toFixed(1)} cm)`}
-              <br />
-            </>
+            </p>
           )}
           {inUseNotes &&
             <div dangerouslySetInnerHTML={{ __html: inUseNotes }} />
