e8a80e665b6178cedc0b6684d7912c65c2351927
loidolt <loidolt@gmail.com>
2023-04-25 16:55:41 -0600

    Clear restock date when switching variant

--- src/templates/ProductsTemplate/index.js
+++ src/templates/ProductsTemplate/index.js
@@ -132,6 +132,7 @@ const ProductPage = ({ data, pageContext }) => {
 
     getProductById(shopifyId).then(result => {
       setProduct(result)
+      setVariantRestockDate(null);
 
       if (result?.variants) {
         const resultVariant =
